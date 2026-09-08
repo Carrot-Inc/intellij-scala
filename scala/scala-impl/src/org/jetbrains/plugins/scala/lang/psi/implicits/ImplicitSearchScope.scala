e353dbec05dc1608f654052f30f69e5e85e14fa6
Sviat Chumakov <svchumakov@gmail.com>
2026-09-08 10:55:59 -0700

    Restore per-scope companion implicit cache: file-level holder made the recursion guard drop results in large files

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/implicits/ImplicitSearchScope.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/implicits/ImplicitSearchScope.scala
@@ -1,7 +1,6 @@
 package org.jetbrains.plugins.scala.lang.psi.implicits
 
 import com.intellij.psi.{PsiElement, PsiFile}
-import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager
 import org.jetbrains.plugins.scala.caches.{BlockModificationTracker, cachedWithRecursionGuard}
 import org.jetbrains.plugins.scala.extensions.{ObjectExt, PsiElementExt, childOf}
 import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScCaseClause
@@ -40,15 +39,12 @@ case class ImplicitSearchScope(representative: PsiElement) {
       new ImplicitParametersProcessor(representative, withoutPrecedence = true).candidatesByLevel
     }
 
-  // Implicits found in the implicit scope of a type (companions of its parts) do not depend on the local
-  // search scope, so they are cached per file rather than per scope representative: a Scala 3 file with many
-  // `for` generators or `case` clauses has many representatives and would otherwise recollect them per scope.
   def cachedImplicitsByType(scType: ScType): Set[ScalaResolveResult] =
     cachedWithRecursionGuard(
       "cachedImplicitsByType",
-      Option(representative.getContainingFile).getOrElse(representative),
+      representative,
       Set.empty[ScalaResolveResult],
-      ScalaPsiManager.instance(representative.getProject).TopLevelModificationTracker,
+      BlockModificationTracker(representative),
       Tuple1(scType)
     ) {
       new ImplicitParametersProcessor(representative, withoutPrecedence = true)
