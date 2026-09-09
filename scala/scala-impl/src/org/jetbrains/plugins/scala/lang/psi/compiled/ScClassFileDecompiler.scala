dc0d26cc677d20e513dd18cfbbb7353dece449a3
Sviat Chumakov <svchumakov@gmail.com>
2026-09-08 18:06:07 -0700

    Keep polymorphic function type parameter clauses out of the enclosing declaration's stub

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/compiled/ScClassFileDecompiler.scala
@@ -39,7 +39,7 @@ object ScClassFileDecompiler {
 
   object ScClsStubBuilder extends compiled.ClsStubBuilder {
 
-    override val getStubVersion = 486
+    override val getStubVersion = 487
 
     override def buildFileStub(content: FileContent): stubs.PsiFileStubImpl[_ <: PsiFile] = {
       val psiFile = decompiledScalaFile(content)
